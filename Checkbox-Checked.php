7790c3d254dbefd4a254d04818c101830219afd1
Aaronkeith <jackhad@gmail.com>
2014-02-21 10:34:02 +0530

    Update Checkbox-Checked.php

--- Checkbox-Checked.php
+++ Checkbox-Checked.php
@@ -1,5 +1,5 @@
 <?php foreach($result as $key => $value){ ?>
       
-      <input type="checkbox" value="" <?php if($value['table_field1']!=''){?>CHECKED<?php } ?> onclick="clickfunc()">
+      <input type="checkbox" value="" <?php if($value['table_field1']!=''){?>CHECKED=CHECKED<?php } ?> onclick="clickfunc()">
       <?php //your array data process here
       <?php }?>
